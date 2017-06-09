e1089c5c6770edac74ca8670c9b53e8c9a3700cc
Sasho-Laptop\Sasho <a.aleksandrov91>
2017-06-09 12:49:49 +0300

    Updated BashSoft Project

--- C# Advanced/BashSoft Project/BashSoft/BashSoftProgram.cs
+++ C# Advanced/BashSoft Project/BashSoft/BashSoftProgram.cs
@@ -1,13 +1,32 @@
-﻿namespace BashSoft
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+namespace BashSoft
 {
     public class BashSoftProgram
     {
         public static void Main()
         {
             //IOManager.TraverseDirectory(@"D:\Development");
-            StudentsRepository.InitializeData();
+            //StudentsRepository.InitializeData();
             //StudentsRepository.GetAllStudentsFromCourse("Unity");
-            StudentsRepository.GetStudentScoreFromCourse("Unity", "Ivan");
+            //StudentsRepository.GetStudentScoreFromCourse("Unity", "Ivan");
+            //Tester.CompareContent(@"C:\Users\Sasho\Desktop\BashSoft-FirstWeek\BashSoft-FirstWeek\03. CSharp-Advanced-Files-And-Directories-Lab\test1.txt", @"C:\Users\Sasho\Desktop\BashSoft-FirstWeek\BashSoft-FirstWeek\03. CSharp-Advanced-Files-And-Directories-Lab\test2.txt");
+            //Tester.CompareContent(@"C:\Users\Sasho\Desktop\BashSoft-FirstWeek\BashSoft-FirstWeek\03. CSharp-Advanced-Files-And-Directories-Lab\test2.txt", @"C:\Users\Sasho\Desktop\BashSoft-FirstWeek\BashSoft-FirstWeek\03. CSharp-Advanced-Files-And-Directories-Lab\test3.txt");
+            //Tester.CompareContent(@"C:\Users\Sasho\Desktop\BashSoft-FirstWeek\BashSoft-FirstWeek\04. CSharp-Advanced-Exception-Handling-Lab\actual.txt", @"C:\Users\Sasho\Desktop\BashSoft-FirstWeek\BashSoft-FirstWeek\04. CSharp-Advanced-Exception-Handling-Lab\expecdted.txt");
+            //IOManager.CreateDirectoryInCurrentFolder("*Sasho");
+            //IOManager.ChangeCurrentDirectoryAbsolute(@"C:\Windows");
+            //IOManager.TraverseDirectory(20);
+            IOManager.ChangeCurrentDirectoryRelative("..");
+            IOManager.ChangeCurrentDirectoryRelative("..");
+            IOManager.ChangeCurrentDirectoryRelative("..");
+            IOManager.ChangeCurrentDirectoryRelative("..");
+            IOManager.ChangeCurrentDirectoryRelative("..");
+            IOManager.ChangeCurrentDirectoryRelative("..");
+            IOManager.ChangeCurrentDirectoryRelative("..");
+            IOManager.ChangeCurrentDirectoryRelative("..");
+            IOManager.ChangeCurrentDirectoryRelative("..");
+
         }
     }
 }
