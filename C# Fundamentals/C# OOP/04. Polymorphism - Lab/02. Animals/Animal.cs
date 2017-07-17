88b7706a9e2fd3289418bd811ea4b521308e8fbc
Sasho-Laptop <a.aleksandrov91@gmail.com>
2017-07-17 13:12:37 +0300

    Added Polymorphism lecture exercises.

---
+++ C# Fundamentals/C# OOP/04. Polymorphism - Lab/02. Animals/Animal.cs
@@ -0,0 +1,16 @@
+﻿public class Animal
+{
+    private string name;
+    private string food;
+
+    public Animal(string name, string food)
+    {
+        this.name = name;
+        this.food = food;
+    }
+
+    public virtual string ExplainMyself()
+    {
+        return $"I am {this.name} and my fovourite food is {this.food}";
+    }
+}
---
+++ C# Fundamentals/C# OOP/04. Polymorphism - Lab/03. Shapes/Rectangle.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+public class Rectangle : Shape
+{
+    private double heigth;
+    private double width;
+
+    public Rectangle(double height, double width)
+    {
+        this.Heigth = height;
+        this.Width = width;
+    }
+
+    public double Heigth
+    {
+        get { return this.heigth; }
+        private set { this.heigth = value; }
+    }
+
+    public double Width
+    {
+        get { return this.width; }
+        private set { this.width = value; }
+    }
+
+    public override double CalculatePerimeter()
+    {
+        return 2 * (this.Heigth + this.Width);
+    }
+
+    public override double CalculateArea()
+    {
+        return this.Heigth * this.Width;
+    }
+
+    public override string Draw()
+    {
+        return base.Draw() + "Rectangle";
+    }
+}
